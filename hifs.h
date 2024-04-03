965ec75b9b2a5262a17b835f6bf4476be6354f73
K. Trotman <kbtrotman@gmail.com>
2024-04-03 16:26:42 -0400

    .

--- hifs.h
+++ hifs.h
@@ -54,10 +54,11 @@ extern struct task_struct *task;
 void hifs_comm_link_up (void);
 int hifs_comm_link_init_change( void );
 int hifs_thread_fn(void *data);
-void hifs_comm_link_up_completed(void);
 void hifs_create_test_inode(void);
 void hifs_wait_on_link(void);
 int hifs_manage_queue_contents(void);
+int hifs_stop_queue_thread(void);
+int hifs_start_queue_thread(void);
 
 /* hi_command_kern_comm_memman.c */
 int hifs_comm_device_open(struct inode *inode, struct file *filp);
