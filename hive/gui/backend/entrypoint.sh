a1606d075c206fb40cdd553c0a72f828214b61e0
Kevin Trotman <kevintrotman@icloud.com>
2025-10-30 17:27:38 -0400

    .

--- hive/gui/backend/entrypoint.sh
+++ hive/gui/backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-exec "$@"
+#python manage.py collectstatic --noinput
+#python manage.py migrate --noinput
+#exec "$@"
