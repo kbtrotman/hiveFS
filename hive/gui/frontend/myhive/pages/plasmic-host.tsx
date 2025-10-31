f7c1512ab75d7604c44d0fc8824928fd1d606299
Kevin Trotman <kevintrotman@icloud.com>
2025-10-31 15:47:14 -0400

    .
    Former-commit-id: 95358cb5d4838f08142923042c783f55cdae1114
    Former-commit-id: eb4f1fe89a63824362a51d08c297440b6e112401

--- hive/gui/frontend/myhive/pages/plasmic-host.tsx
+++ hive/gui/frontend/myhive/pages/plasmic-host.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '@/plasmic-init';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function PlasmicHost() {
   return PLASMIC && <PlasmicCanvasHost />;
+}
+
+
+function AppRoot() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hive-setup" element={<PlasmicCanvasHost />} />
+        <Route path="/hive-mon" element={<PlasmicCanvasHost />} />
+        {/* Other routes for your application */}
+      </Routes>
+    </Router>
+  );
 }
