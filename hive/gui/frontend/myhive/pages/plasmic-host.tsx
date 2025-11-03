f04271ce58efed5152ada9fde2ab3126e403b751
Kevin Trotman <kevintrotman@icloud.com>
2025-11-03 13:45:20 -0500

    .

--- hive/gui/frontend/myhive/pages/plasmic-host.tsx
+++ hive/gui/frontend/myhive/pages/plasmic-host.tsx
@@ -1,21 +1,15 @@
 import * as React from 'react';
-import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
-import { PLASMIC } from '@/plasmic-init';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/host';
+
+// You can register any code components that you want to use here; see
+// https://docs.plasmic.app/learn/code-components-ref/
+// And configure your Plasmic project to use the host url pointing at
+// the /plasmic-host page of your nextjs app (for example,
+// http://localhost:3000/plasmic-host).  See
+// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
+
+// registerComponent(...)
 
 export default function PlasmicHost() {
-  return PLASMIC && <PlasmicCanvasHost />;
-}
-
-
-function AppRoot() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/hive-setup" element={<PlasmicCanvasHost />} />
-        <Route path="/hive-mon" element={<PlasmicCanvasHost />} />
-        {/* Other routes for your application */}
-      </Routes>
-    </Router>
-  );
+  return <PlasmicCanvasHost />;
 }
