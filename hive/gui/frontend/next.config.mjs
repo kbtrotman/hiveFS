2e7ee8f0c892eae25be4e2aaadd110d08588685e
Kevin Trotman <kevintrotman@icloud.com>
2025-11-12 21:58:59 -0500

    .

--- hive/gui/frontend/next.config.mjs
+++ hive/gui/frontend/next.config.mjs
@@ -1,11 +1,35 @@
+// next.config.mjs
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  experimental: { esmExternals: 'loose' },
+  trailingSlash: false,
+
+  async redirects() {
+    return [];
   },
-  trailingSlash: true,
-  reactStrictMode: true,
+
+  transpilePackages: [
+    'antd',
+    '@ant-design',
+    '@ant-design/icons',
+    'rc-util',
+    'rc-tree',
+    'rc-motion',
+    'rc-virtual-list',
+    'rc-resize-observer',
+    'rc-picker',
+  ],
+
+  webpack: (config) => {
+    config.resolve.alias['rc-util/es'] = 'rc-util/lib';
+    config.resolve.alias['rc-tree/es'] = 'rc-tree/lib';
+    config.resolve.alias['rc-motion/es'] = 'rc-motion/lib';
+    config.resolve.alias['rc-virtual-list/es'] = 'rc-virtual-list/lib';
+    config.resolve.alias['rc-resize-observer/es'] = 'rc-resize-observer/lib';
+    config.resolve.alias['rc-picker/es'] = 'rc-picker/lib';  // <-- add this
+    return config;
+  },
 };
 
 export default nextConfig;
